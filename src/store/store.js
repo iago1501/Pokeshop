3ff63c20af07567da4b4ced74b78ec6666791de1
Iago Espinoza <iespinoza632@beijaflore.com>
2020-05-26 16:17:51 -0300

    :package: thunk added

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,11 @@ import { persistStore } from 'redux-persist';
 
 import rootReducer from './ducks/root-reducer';
 
-const middleWares = [logger];
+const middleWares = [thunk];
+
+if (process.env.NODE_ENV === "development") {
+    middleWares.push(logger);
+  }
 
 export const store = createStore(rootReducer, applyMiddleware(...middleWares));
 export const persistor = persistStore(store);
