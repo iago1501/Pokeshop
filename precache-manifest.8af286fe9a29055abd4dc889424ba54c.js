10fce3b4021dded4de64be6ff53306a0f7690160
Iago Espinoza <iespinoza632@beijaflore.com>
2020-06-01 11:24:44 -0300

    Updates

---
+++ precache-manifest.8af286fe9a29055abd4dc889424ba54c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd394c6a2e7579ae63aeb432808eb16",
+    "url": "/pokeshop/index.html"
+  },
+  {
+    "revision": "27fb74ea6a0e9b6164d4",
+    "url": "/pokeshop/static/css/main.1c6f90c3.chunk.css"
+  },
+  {
+    "revision": "ed40412c87325371b1eb",
+    "url": "/pokeshop/static/js/2.70866475.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/pokeshop/static/js/2.70866475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27fb74ea6a0e9b6164d4",
+    "url": "/pokeshop/static/js/main.02bb1364.chunk.js"
+  },
+  {
+    "revision": "9056472b59d39a5842d9",
+    "url": "/pokeshop/static/js/runtime-main.53a44e51.js"
+  },
+  {
+    "revision": "63da3b8bd9f364ea2b43218131c18674",
+    "url": "/pokeshop/static/media/fairy.63da3b8b.png"
+  },
+  {
+    "revision": "2d3231bc2e6b25e4fb103d1e34ccd520",
+    "url": "/pokeshop/static/media/fire.2d3231bc.png"
+  },
+  {
+    "revision": "1a9d5f7bbb54ed7bf47911b463ae78a5",
+    "url": "/pokeshop/static/media/ice.1a9d5f7b.png"
+  },
+  {
+    "revision": "d96f3360fc748568472f2419fddbdcae",
+    "url": "/pokeshop/static/media/normal.d96f3360.png"
+  },
+  {
+    "revision": "6e5393a64ca23c455e8568d409e82da6",
+    "url": "/pokeshop/static/media/pokemonhollow.6e5393a6.ttf"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/pokeshop/static/media/pokemonsolid.36d26dbc.ttf"
+  },
+  {
+    "revision": "74496d9086d97aaeeafb3085e9957668",
+    "url": "/pokeshop/static/media/pressstart2p.74496d90.ttf"
+  },
+  {
+    "revision": "d2d4fec628771f1a2dbf333e6a758db9",
+    "url": "/pokeshop/static/media/psychic.d2d4fec6.png"
+  },
+  {
+    "revision": "2029fe65c803e04b24ed1d56a6a5ae08",
+    "url": "/pokeshop/static/media/water.2029fe65.png"
+  }
+]);
